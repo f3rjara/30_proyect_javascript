08af11242b103d21b63f2eae43b8b8e9bfe18475
fernando.jaramillo <fernando.jaramillo@branch.com.co>
2021-05-11 23:43:19 -0500

    add assets for proyect and config filepath

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./styles.scss";
+import logo from "./assets/img/github.png";
+import fondo from "./assets/img/fondo.jpg";
+
+document.querySelector('#app').innerHTML = `<img src="${logo}" alt "logo">`;
+
+const title = document.querySelector('.title');
