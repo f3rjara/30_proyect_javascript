ac2a4b5a04adec4f54f9b96a2b08015a512eadf0
fernando.jaramillo <fernando.jaramillo@branch.com.co>
2021-05-12 19:35:42 -0500

    add assets for proyect

--- src/assets/js/assets.js
+++ src/assets/js/assets.js
@@ -1,2 +1,3 @@
-import logo from "./../img/github.png";
-import fondo from "./../img/fondo.jpg";
+import img_github from "./../img/github.png";
+import img_fondo from "./../img/fondo.jpg";
+import img_js30 from "./../img/js_30.png";
